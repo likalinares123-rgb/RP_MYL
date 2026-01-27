98eafe734319638ce8f8570527805978104dc97b
likalinares123-rgb <likalinares123@gmail.com>
2026-01-27 01:34:03 +0000

    refactor: ordenar modulo clientes v2

--- clientes/repository.py
+++ clientes/repository.py
@@ -3,43 +3,22 @@ from db import get_connection
 def crear_tabla_clientes():
     conn = get_connection()
     cur = conn.cursor()
-
     cur.execute("""
-    CREATE TABLE IF NOT EXISTS clientes (
-        id SERIAL PRIMARY KEY,
-        nombre VARCHAR(100),
-        email VARCHAR(100)
-    )
+        CREATE TABLE IF NOT EXISTS clientes (
+            id SERIAL PRIMARY KEY,
+            nombre TEXT NOT NULL,
+            email TEXT NOT NULL
+        )
     """)
-
     conn.commit()
     cur.close()
     conn.close()
-
-
-def insertar_cliente(nombre, email):
-    conn = get_connection()
-    cur = conn.cursor()
-
-    cur.execute(
-        "INSERT INTO clientes (nombre, email) VALUES (%s, %s)",
-        (nombre, email)
-    )
-
-    conn.commit()
-    cur.close()
-    conn.close()
-
 
 def obtener_clientes():
     conn = get_connection()
     cur = conn.cursor()
-
     cur.execute("SELECT id, nombre, email FROM clientes ORDER BY id DESC")
-    clientes = cur.fetchall()
-
+    datos = cur.fetchall()
     cur.close()
     conn.close()
-    return clientes
-
-
+    return datos
