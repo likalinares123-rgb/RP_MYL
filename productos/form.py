62ec1fc685571eea3492214252ac449927775331
likalinares123-rgb <likalinares123@gmail.com>
2026-01-29 02:58:37 +0000

    feat: se agrego prductos

---
+++ productos/form.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from db import get_connection
+
+productos_form_bp = Blueprint(
+    "productos_form",
+    __name__,
+    url_prefix="/productos"
+)
+
+# 🟢 Mostrar formulario
+@productos_form_bp.route("/nuevo", methods=["GET"])
+def nuevo_producto():
+    return render_template("productos/form.html")
+
+# 🟢 Guardar cliente
+@productos_form_bp.route("/guardar", methods=["POST"])
+def guardar_producto():
+    nombre = request.form["nombre"]
+    descripcion = request.form["descripcion"]
+
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute(
+        "INSERT INTO productos (nombre, descripcion) VALUES (%s, %s)",
+        (nombre, descripcion)
+    )
+    conn.commit()
+    cur.close()
+    conn.close()
+
+ 
+
+    return redirect(url_for("productos.listado_productos"))
---
+++ productos/repository.py
@@ -0,0 +1,24 @@
+from db import get_connection
+
+def crear_tabla_clientes():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS productos (
+            id SERIAL PRIMARY KEY,
+            nombre TEXT NOT NULL,
+            descripción TEXT NOT NULL
+        )
+    """)
+    conn.commit()
+    cur.close()
+    conn.close()
+
+def obtener_clientes():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT id, nombre, descripción FROM productos ORDER BY id DESC")
+    datos = cur.fetchall()
+    cur.close()
+    conn.close()
+    return datos
